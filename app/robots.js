8ecdb369d5e45ab9bc255262541a81895deb8394
ratchanon2318 <ratchanon.noknoy2318@gmail.com>
2026-01-14 10:31:49 +0700

    sitemap

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://kppmch-register.vercel.app/sitemap.xml',
+  }
+}
